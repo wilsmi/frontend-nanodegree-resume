456a432bc53af0ea56a2465d403f28157061d4c7
wilsmi <william.smith3@gmail.com>
2017-02-24 10:51:48 -0500

    Update helper.js

--- js/helper.js
+++ js/helper.js
@@ -1,17 +1,3 @@
-/*
-
-This file contains all of the code running in the background that makes resumeBuilder.js possible. We call these helper functions because they support your code in this course.
-
-Don't worry, you'll learn what's going on in this file throughout the course. You won't need to make any changes to it until you start experimenting with inserting a Google Map in Problem Set 3.
-
-Cameron Pittman
-*/
-
-
-/*
-These are HTML strings. As part of the course, you'll be using JavaScript functions
-replace the %data% placeholder text you see in them.
-*/
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
 var HTMLheaderRole = '<span>%data%</span><hr>';
 
@@ -48,7 +34,7 @@ var HTMLschoolDegree = ' -- %data%</a>';
 var HTMLschoolDates = '<div class="date-text">%data%</div>';
 var HTMLschoolLocation = '<div class="location-text">%data%</div>';
 var HTMLschoolMajor = '<em><br>Major: %data%</em>';
-var HTMLschoolMinor = "&nbsp; &frasl; &nbsp;<em>Minor: %data%</em>"
+var HTMLschoolFocus = "&nbsp; &frasl; &nbsp;<em>Minor: %data%</em>"
 
 var HTMLonlineClasses = "<h3 class='online-classes'>Online Classes</h3>";
 var HTMLonlineTitle = '<a href="#">%data%';
